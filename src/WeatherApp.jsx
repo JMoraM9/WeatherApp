0ccb7daf4b14133664239b79266824d3d694f415
Jose Mora <jmoramt@gmail.com>
2021-06-24 22:59:48 -0500

    changes in package.json y weatherapp

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from 'react'
-import { TodayScreen } from './components/TodayScreen';
-
-import useAxios from 'axios-hooks'
-=======
 import React, { useReducer } from 'react'
 import { WeekScreen } from './components/forecast/WeekScreen';
 import { HighlightsScreen } from './components/highlights/HighlightsScreen';
 import { TodayScreen } from './components/TodayScreen';
->>>>>>> temp
 import './styles/styles.scss';
 import { weatherContext } from './weather/weatherContext';
 import { weatherReducer } from './weather/weatherReducer';
@@ -16,47 +9,6 @@ import { weatherReducer } from './weather/weatherReducer';
 
 export const WeatherApp = () => {
 
-<<<<<<< HEAD
-    const [coords, setCoords] = useState({});
-    const [loadingCoords, setLoadingCoords] = useState(false);
-    const [city, setCity] = useState("Medellin");
-
-    const Mapbox_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${coords.lng}, ${coords.lat}.json?access_token=pk.eyJ1IjoianJtb3JhbSIsImEiOiJja3ByNWo5ZGIwMXNqMnVtdmc3YWNta3l5In0.Sb5b6IfOFX1286wMXBqNxA&limit=1&types=place`;
-
-    const OpenWeather_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f0fb9d9d214577295562f8baa54b5fc2&units=metric`;
-
-    const [, getLocation] = useAxios( Mapbox_url, { manual: true } );
-
-    const [{ data }, getWeather, manualCancel] = useAxios( OpenWeather_url );
-
-    useEffect(() => {
-        getWeather()
-            .catch( (err) => console.log(err) );
-        
-    }, [city])
-
-    useEffect(() => {
-        if (coords.lat){
-            getLocation()
-                .then( ({data: {features}}) => {
-                    //Obtener el clima
-                    setCity( features[0].text )
-                })
-                .catch( err => console.log(err))
-        }
-    }, [coords, getLocation, getWeather])
-
-    console.log(coords);
-
-    return (
-        <>
-            <TodayScreen
-                weather={ data?.weather }
-                city={ city }
-                setCoords={ setCoords } 
-                setLoadingCoords={ setLoadingCoords }
-            />
-=======
     const initialState = {
         city: "Medellin",
         coords: {
@@ -76,7 +28,6 @@ export const WeatherApp = () => {
                 <HighlightsScreen />
             </weatherContext.Provider>
 
->>>>>>> temp
         </>
     )
 }
